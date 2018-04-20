bcb179e900e2259f7c57836235bdd9c975fc7e7a
Jules Waldhart <jules.waldhart@laas.fr>
2018-04-20 13:46:12 +0200

    updates

--- move4d/run.bash
+++ move4d/run.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 source /move4d/catkin_ws/devel/setup.bash
-export HOME_MOVE4D=/move4d
+export HOME_MOVE4D=/move4d/data/move4d
 roslaunch pointing_planner pointing_planner_dbg.launch
